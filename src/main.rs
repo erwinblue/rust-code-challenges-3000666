c3fa0e9914170f3cec8ff11d24d2eb080a80710f
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-11 16:29:55 -0500

    Add solution to chapter 2 exercise and advanced exercise 1.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,88 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!();
-}
 
-// advanced 1: use generic types
-// fn unique(a: Vec<T>) -> Vec<T> {
-//     todo!();
-// }
+use std::cmp::PartialEq;
+
+/*fn unique(a: Vec<i32>) -> Vec<i32> {
+    //todo!();
+
+    /* 
+        Solution #1: Own Implementation of unique 
+    */
+
+    /*let mut ret_vec: Vec<i32> = Vec::new();
+    match a.is_empty() {
+       true => return a,
+       false => ()
+    };
+    for (x, y) in a.iter().enumerate() {
+        //println!("-----\n\nElem {:?} value {:?}", x, *y);
+        match x {
+            0 => {ret_vec.push(*y); continue;},
+            1.. => {}
+        };
+        let mut is_unique = true;
+        for z in ret_vec.iter() {
+            if *y == *z {
+                is_unique = false;
+            }
+        } 
+        if is_unique {
+            ret_vec.push(*y);
+        }
+    }
+    ret_vec.sort();
+    return ret_vec;*/
+
+    /* 
+        Solution #2: Use Rust modules for simple Vec<i32> 
+    */
+    let mut v = a.clone();
+    v.sort();
+    v.dedup();
+    v
+}
+*/
+
+/*
+    Advanced 1: use generic types solution
+ */
+fn unique<T: Copy + Ord + PartialEq>(a: Vec<T>) -> Vec<T> {
+    //todo!();
+    /* 
+        Solution #1: Own Implementation of unique 
+    */
+    /*let mut ret_vec: Vec<T> = Vec::new();
+    match a.is_empty() {
+       true => return a,
+       false => ()
+    };
+    for (x, y) in a.iter().enumerate() {
+        //println!("-----\n\nElem {:?} value {:?}", x, *y);
+        match x {
+            0 => {ret_vec.push(*y); continue;},
+            1.. => {}
+        };
+        let mut is_unique = true;
+        for z in ret_vec.iter() {
+            if *y == *z {
+                is_unique = false;
+            }
+        } 
+        if is_unique {
+            ret_vec.push(*y);
+        }
+    }
+    ret_vec.sort();
+    return ret_vec;*/
+
+    /* 
+        Solution #2: Use Rust modules for simple Vec<i32> 
+    */
+    let mut v = a.clone();
+    v.sort();
+    v.dedup();
+    v
+
+}
 
 // advanced 2: keep items in order
 // fn unique(a: Iterable<T>) -> Vec<T> {
@@ -26,8 +103,10 @@ fn main() {
 
 #[test]
 fn empty_list() {
-    let input = vec![];
-    let expected_output = vec![];
+    //let input = vec![];
+    //let expected_output = vec![];
+    let input: Vec<i32>= vec![];
+    let expected_output = Vec::new();
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
 }
