9c961b808bde0223c098b48b63e1a9ddff6d9e35
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-10 21:44:01 -0500

    Add solution to chapter 1 exercise 1.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+    //todo!();
+    return match (a.len(), a.len() % 2) {
+        // empty list
+        (0, _) => None,
+        // even list
+        (_, 0) => {
+            let med = a.len() / 2;
+            let mut v = a.clone();
+            v.sort_by(|x, y|x.partial_cmp(y).unwrap());
+            let num = (v.get(med-1).unwrap_or(&0.0) + v.get(med).unwrap_or(&0.0)) / 2.0;
+            Some(num)
+        }, 
+        // odd list
+        (_, 1..) => {
+            let med = (a.len() - 1)/2;
+            let mut v = a.clone();
+            v.sort_by(|x, y|x.partial_cmp(y).unwrap());
+            let num = v.get(med).unwrap_or(&0.0);
+            Some(*num)
+        }
+    }
 }
 
 fn main() {
