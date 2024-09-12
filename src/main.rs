09430b2f21a0fb556d9e00b4b1e7a23654b24cb6
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-12 16:30:08 -0500

    Add solution to advanced 2 exercise

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,8 @@ use std::cmp::PartialEq;
 
 /*fn unique(a: Vec<i32>) -> Vec<i32> {
     //todo!();
-
     /* 
-        Solution #1: Own Implementation of unique 
-    */
-
-    /*let mut ret_vec: Vec<i32> = Vec::new();
-    match a.is_empty() {
-       true => return a,
-       false => ()
-    };
-    for (x, y) in a.iter().enumerate() {
-        //println!("-----\n\nElem {:?} value {:?}", x, *y);
-        match x {
-            0 => {ret_vec.push(*y); continue;},
-            1.. => {}
-        };
-        let mut is_unique = true;
-        for z in ret_vec.iter() {
-            if *y == *z {
-                is_unique = false;
-            }
-        } 
-        if is_unique {
-            ret_vec.push(*y);
-        }
-    }
-    ret_vec.sort();
-    return ret_vec;*/
-
-    /* 
-        Solution #2: Use Rust modules for simple Vec<i32> 
+        Solution: Use Rust modules for simple Vec<i32> 
     */
     let mut v = a.clone();
     v.sort();
@@ -45,7 +16,7 @@ use std::cmp::PartialEq;
 /*
     Advanced 1: use generic types solution
  */
-fn unique<T: Copy + Ord + PartialEq>(a: Vec<T>) -> Vec<T> {
+/*fn unique<T: Copy + Ord + PartialEq>(a: Vec<T>) -> Vec<T> {
     //todo!();
     /* 
         Solution #1: Own Implementation of unique 
@@ -82,14 +53,39 @@ fn unique<T: Copy + Ord + PartialEq>(a: Vec<T>) -> Vec<T> {
     v.dedup();
     v
 
-}
+}*/
 
 // advanced 2: keep items in order
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+fn unique<T: Copy + Ord + PartialEq>(a: Vec<T>) -> Vec<T> {
+    //todo!();
+    /* 
+        Solution: Using Iterators
+    */
+    let mut ret_vec: Vec<T> = Vec::new();
+    match a.is_empty() {
+       true => return a,
+       false => ()
+    };
+    for (x, y) in a.iter().enumerate() {
+        match x {
+            0 => {ret_vec.push(*y); continue;},
+            1.. => {}
+        };
+        let mut is_unique = true;
+        for z in ret_vec.iter() {
+            if *y == *z {
+                is_unique = false;
+            }
+        } 
+        if is_unique {
+            ret_vec.push(*y);
+        }
+    }
+    ret_vec.sort();
+    return ret_vec;
+}
 
-// advanced 3: use iterators
+// advanced 3: use iterators --> THIS IS SAME IS advanced 2 without line#84
 // fn unique(a: Iterable<T>) -> Vec<T> {
 //     todo!();
 // }
