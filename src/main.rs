4859e6c7e4d742517df797c09eb328f51120b255
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-13 13:49:54 -0500

    Add different solutions to exercise 4.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-fn sort_usernames<T: AsRef<str>>(usernames: &mut Vec<T>) {
-    todo!();
+fn sort_usernames<T: AsRef<str> + Ord>(usernames: &mut Vec<T>) {
+    //todo!();
+
+    // solution#1,
+    /*usernames.sort_by(|a, b|a.as_ref().to_ascii_lowercase()
+        .partial_cmp(&b.as_ref().to_ascii_lowercase())
+        .expect("Cannot compare!"));*/
+
+    // or solution#2,
+    //usernames.sort_by(|a, b|a.as_ref().to_string().to_ascii_lowercase()
+    //    .partial_cmp(&b.as_ref().to_string().to_ascii_lowercase())
+    //    .expect("Cannot compare!"));
+
+    // or, instructor's solution
+    usernames.sort_by_cached_key(|k|k.as_ref().to_lowercase());
 }
 
 fn main() {
