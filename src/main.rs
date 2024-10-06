e4358dd02569d07897afe69148ed03a55c2bdfbb
Erwin Coder 9 <erwincoder9@gmail.com>
2024-10-06 13:20:38 -0500

    Solution for chapter 1 challenge 9 exercise.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use chrono::*;
+
+// Assume date specified are in format YYYY-MM-DD and are valid dates
 fn weeks_between(a: &str, b: &str) -> i32 {
-    todo!()
+    //todo!()
+    let date1 = match NaiveDate::parse_from_str(a, "%Y-%m-%d") {
+        Ok(d1) => d1,
+        Err(_) => panic!("ERROR: Cannot parse date string {:?}", a)
+    };
+    //println!("date1: {:?}", date1);
+    let date2 = match NaiveDate::parse_from_str(b, "%Y-%m-%d") {
+        Ok(d2) => d2,
+        Err(_)=> panic!("ERROR: Cannot parse date string {:?}", b)
+    };
+    //println!("date2: {:?}", date2);
+
+    let td = date2.signed_duration_since(date1);
+    //println!("TD: {:?}", td);
+    //let weeks = td.num_weeks() as i32;
+    td.num_weeks() as i32
 }
 
 fn main() {
