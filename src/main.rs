94338b1909182aec05b49a34918596e702bdbc07
Erwin Coder 9 <erwincoder9@gmail.com>
2024-10-14 13:30:56 -0500

    Solution to challenge 11.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,85 @@
+use core::fmt;
 use std::str::FromStr;
 
+#[derive(Debug, PartialEq)]
+struct ParseIsbnError {
+    error: String
+}
+
+impl fmt::Display for ParseIsbnError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.error)
+    }
+}
+
+#[derive(Debug, PartialEq)]
 struct Isbn {
     raw: String,
     digits: Vec<u8>,
 }
 
 impl FromStr for Isbn {
-    type Err = (); // TODO: replace with appropriate type
+    //type Err = (); // TODO: replace with appropriate type
+    type Err = ParseIsbnError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!();        
+        //todo!();        
+
+        // Convert string to a vector of u8 digits
+        /*let isbn13: Vec<u8> = s.replace("-","")
+            .chars()
+            .into_iter()
+            .map(|x| {
+                x.to_string().parse::<u8>()
+                    .map_err(|_| ParseIsbnError { error: "Cannot parse number from ISBN".to_string() } )
+                    .unwrap_or(0u8)
+                })
+            .collect();*/
+        let mut parse_err: bool = false;
+        let isbn13: Vec<u8> = s.replace("-", "").chars()
+            .map(|x| {
+                let y = match x {
+                    '0'..='9' => match x.to_digit(10) {
+                        Some(a) => a as u8,
+                        None => return Err(ParseIsbnError { error: "Cannot parse ISBN number!".to_string() })
+                    },
+                    _ => return Err(ParseIsbnError { error: "Cannot parse ISBN number!".to_string() })
+                };
+                Ok(y)
+            })
+            // a lazy way to unwrap without using unwrap, or its variants...
+            .map(|x| {
+               let y = match x {
+                   Ok(z) => z,
+                   Err(_) => { parse_err = true; 0u8 }
+               };
+               y
+            })
+            .collect();
+
+        if parse_err {
+            return Err(ParseIsbnError { error: "Cannot parse ISBN number!".to_string() });
+        } else if isbn13.len() > 13 {
+            return Err(ParseIsbnError { error: "ISBN number is too long".to_string() });
+        } else if isbn13.len() < 13 {
+            return Err(ParseIsbnError { error: "ISBN number is too short".to_string() });
+        }
+
+        // Get the ISBN13 checksum number from provided ISBN
+        let check_number = isbn13[12];
+
+        // Compute the checksum number from the ISBN digits
+        let computed_check = calculate_check_digit(&isbn13[0..=11]);
+
+        // Check the computed and provided check sum
+        if check_number != computed_check {
+            return Err(ParseIsbnError { error: "Invalid ISBN number!".to_string() });
+        } else {
+            return Ok(Isbn {
+                raw: s.to_string(),
+                digits: isbn13
+            })
+        }
     }
 }
 
@@ -21,7 +91,24 @@ impl std::fmt::Display for Isbn {
 
 // https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-13_check_digit_calculation
 fn calculate_check_digit(digits: &[u8]) -> u8 {
-    todo!()
+    //todo!()
+    let mut count = 1u8;
+    let sum_of_12 = digits.iter().fold(0,|acc: u32, x| {
+            let y = match  count % 2 {
+                0 => x * 3,
+                _ => x * 1
+            };
+            count += 1;
+            acc + y as u32
+        });
+    let rem = 10 - (sum_of_12 % 10);
+
+    // rem is assumed to be 0 to 10
+    if rem == 10 {
+        return 0u8;
+    } else {
+        return rem as u8;
+    }
 }
 
 fn main() {
@@ -47,4 +134,11 @@ fn can_correctly_calculate_check_digits() {
 #[test]
 fn rust_in_action() {
     let _: Isbn = "978-3-16-148410-0".parse().unwrap();
+}
+
+#[test]
+fn rust_invalid_isbn() {
+    let invalid: Result<_, ParseIsbnError> = Err(ParseIsbnError { 
+        error: "Cannot parse ISBN number!".to_string() });
+    assert_eq!("978-3-16-14841C-0".parse::<Isbn>(), invalid);
 }
