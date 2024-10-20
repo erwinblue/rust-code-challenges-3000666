4b982c3354a63096c56234789c3f1a61c58b4474
Erwin Coder 9 <erwincoder9@gmail.com>
2024-10-20 12:39:16 -0500

    Solution to exercise 15 chapter 1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,26 @@ use chrono::NaiveDate;
 /// Parses a string that represents a date. When a date
 /// is unable to be determined, return `None`. 
 fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
-    todo!();
+    //todo!();
+    let supported_date_formats: Vec<&str> = vec![
+        "%Y-%m-%d",         // 2010-12-11
+        "%Y/%b/%d",         // 2010/Dec/11
+        "%d.%b.%Y",         // 11.Dec.2010
+        "%b.%d.%Y",         // Dec.11.2010
+        "%Y $b %d",         // 2010 Dec 11
+        "%Y/%B/%d",         // 2010/December/11
+    ];
+    let mut parsed_date: Option<NaiveDate> = None;
+    for date_fmt in supported_date_formats.iter() {
+        let dt = NaiveDate::parse_from_str(text.trim(), *date_fmt);
+        if dt.is_err() {
+            continue;
+        } else {
+            parsed_date = dt.ok();
+            break;
+        }
+    }
+    parsed_date
 }
 
 fn main() {
@@ -23,22 +42,34 @@ fn main() {
 
 #[test]
 fn ymd_hyphen() {
-    assert_eq!(flexible_date_parse("2010-12-11"), Some(NaiveDate::from_ymd(2010, 12, 11)))
+    assert_eq!(flexible_date_parse("2010-12-11"), Some(match NaiveDate::from_ymd_opt(2010, 12, 11) {
+        Some(d) => d,
+        None => panic!("error!")
+    }))
 }
 
 #[test]
 fn ymd_slash() {
-    assert_eq!(flexible_date_parse("1999/Mar/02"), Some(NaiveDate::from_ymd(1999, 3, 2)))
+    assert_eq!(flexible_date_parse("1999/Mar/02"), Some(match NaiveDate::from_ymd_opt(1999, 3, 2) {
+        Some(d) => d,
+        None => panic!("error!")
+    }))
 }
 
 #[test]
 fn dmy_dot() {
-    assert_eq!(flexible_date_parse("01.Mar.2021"), Some(NaiveDate::from_ymd(2021, 3, 1)))
+    assert_eq!(flexible_date_parse("01.Mar.2021"), Some(match NaiveDate::from_ymd_opt(2021, 3, 1) {
+        Some(d) => d,
+        None => panic!("error!")
+    }))
 }
 
 #[test]
 fn mdy_dot() {
-    assert_eq!(flexible_date_parse("Apr.05.2021"), Some(NaiveDate::from_ymd(2021, 4, 5)))
+    assert_eq!(flexible_date_parse("Apr.05.2021"), Some(match NaiveDate::from_ymd_opt(2021, 4, 5) {
+        Some(d) => d,
+        None => panic!("error!")
+    }))
 }
 
 #[test]
